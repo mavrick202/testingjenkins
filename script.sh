dac5d92e7cdb3a5a9eca65e5512af5f508ab11bc
Mavrick-DevOps <mavrick202@gmail.com>
2019-06-21 07:32:31 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "RUNNING THE SCRIPT NOW...!!"
+echo "----------------------------------------"
+$OS=$(cat /etc/lsb-release)
+echo "The OS details are: $OS"
+echo "----------------------------------------"
