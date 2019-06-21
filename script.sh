ced74f4bab40d2901fefc5c28e34e2b2d38d3ceb
Mavrick-DevOps <mavrick202@gmail.com>
2019-06-21 07:41:56 +0530

    Edited OS Paramater

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "RUNNING THE SCRIPT NOW...!!"
 echo "----------------------------------------"
-$OS=$(cat /etc/lsb-release)
+OS=$(cat /etc/lsb-release)
 echo "The OS details are: $OS"
 echo "----------------------------------------"
